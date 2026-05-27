287f7cd0e78b56691c6948edf1599375c3b4d3c9
Demolisher98 <puppalaomprabhas@gmail.com>
2026-05-27 20:05:53 +0530

    Fixing Hosting

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 import { qrcode } from 'vite-plugin-qrcode' // <-- Import the plugin
 
 export default defineConfig({
+  base: '/internship-project/',
   plugins: [
     basicSsl(),
     qrcode() // <-- Activate it here
   ],
-  base: '/internship-project/',
   server: {
     host: true,
     port: 5173
