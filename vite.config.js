62a88fb2b8fa1e8d07a474af1759f08b0d0e955f
Demolisher98 <puppalaomprabhas@gmail.com>
2026-05-28 11:41:57 +0530

    fixing react

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or '@vitejs/plugin-react-swc' if using react
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { qrcode } from 'vite-plugin-qrcode'
 
-// 💡 NOTICE the ({ command }) parameters inside the defineConfig call below
 export default defineConfig(({ command }) => {
   return {
     base: '/internship-project/',
-    plugins: command === 'serve' ? [react(), basicSsl(), qrcode()] : [react()],
+    // Only load local development tools when running 'npm run dev' (serve)
+    plugins: command === 'serve' ? [basicSsl(), qrcode()] : [],
   }
 })
